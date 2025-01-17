63dc37c96983117df0a0d0b8febea61f334eea6b
Lucas Gomes <gomeszx.lucas@gmail.com>
2025-01-17 13:31:58 -0300

    Criando tabelas no h2 da proprietes

--- desafio/src/main/java/com/desafiomodelodominio/desafio/entities/Category.java
+++ desafio/src/main/java/com/desafiomodelodominio/desafio/entities/Category.java
@@ -1,4 +1,33 @@
 package com.desafiomodelodominio.desafio.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String description;
+
+    public Category() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
